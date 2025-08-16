c53ce0247b284cfdd582ca628149fe6d54b10f25
Arwei0 <henson0220@gmail.com>
2025-08-16 23:44:18 +0800

    stock

--- test_push.py
+++ test_push.py
@@ -1,12 +1,13 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import os
 
-# 從環境變數拿 LINE access token
-CHANNEL_ACCESS_TOKEN = os.getenv("LQDJgnexyskwDiH2nRQge/V0yrXMwNv2XEbFP0wQAL43ma8mgvOWcmydW9RgwE+Jxtlhj59bvppbQvoLPCoBkvzN0JuKWLWmxc1eHQ8EW9de4xYtXtUBRdc1X+o6gaznXZF9w2JqTZlCUqskqKmJaYAdB04t89/1O/w1cDnyilFU=")
+# 把這裡換成你 LINE Developers 後台的 Channel access token (長長的一串字)
+CHANNEL_ACCESS_TOKEN = "你的ChannelAccessToken"
+
+# 這裡換成你自己的 UserId（像 Ueeb38edab1a74313586d1bc43f792dbe）
+USER_ID = "你的UserId"
+
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+line_bot_api.push_message(USER_ID, TextSendMessage(text="Hello from test_push.py!"))
 
-USER_ID = "Ueeb38edab1a74313586d1bc43f792dbe"
-
-line_bot_api.push_message(USER_ID, TextSendMessage(text="測試推播成功！"))
-print("推播完成")
+print("測試訊息已送出！")
