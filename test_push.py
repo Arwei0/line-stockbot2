2c3804a97f8fc3fb1aaa304cb27452ee1a71c242
Arwei0 <henson0220@gmail.com>
2025-08-16 23:37:07 +0800

    stock

--- test_push.py
+++ test_push.py
@@ -3,7 +3,7 @@ from linebot.models import TextSendMessage
 import os
 
 # 從環境變數拿 LINE access token
-CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_ACCESS_TOKEN = os.getenv("LQDJgnexyskwDiH2nRQge/V0yrXMwNv2XEbFP0wQAL43ma8mgvOWcmydW9RgwE+Jxtlhj59bvppbQvoLPCoBkvzN0JuKWLWmxc1eHQ8EW9de4xYtXtUBRdc1X+o6gaznXZF9w2JqTZlCUqskqKmJaYAdB04t89/1O/w1cDnyilFU=")
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 USER_ID = "Ueeb38edab1a74313586d1bc43f792dbe"
