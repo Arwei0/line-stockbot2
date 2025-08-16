ffdd13f3b5317790a80e652fea3850c4879eca02
Arwei0 <henson0220@gmail.com>
2025-08-16 23:56:25 +0800

    stock2

--- test_push.py
+++ test_push.py
@@ -4,10 +4,10 @@ import sys
 sys.stdout.reconfigure(encoding='utf-8')
 
 # 把這裡換成你 LINE Developers 後台的 Channel access token (長長的一串字)
-CHANNEL_ACCESS_TOKEN = "你的ChannelAccessToken"
+CHANNEL_ACCESS_TOKEN = "QDJgnexyskwDiH2nRQge/V0yrXMwNv2XEbFP0wQAL43ma8mgvOWcmydW9RgwE+Jxtlhj59bvppbQvoLPCoBkvzN0JuKWLWmxc1eHQ8EW9de4xYtXtUBRdc1X+o6gaznXZF9w2JqTZlCUqskqKmJaYAdB04t89/1O/w1cDnyilFU="
 
 # 這裡換成你自己的 UserId（像 Ueeb38edab1a74313586d1bc43f792dbe）
-USER_ID = "你的UserId"
+USER_ID = "Ueeb38edab1a74313586d1bc43f792dbe"
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 line_bot_api.push_message(USER_ID, TextSendMessage(text="Hello from test_push.py!"))
