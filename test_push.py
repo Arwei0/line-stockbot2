c87c77b7a1224d9472b85d235e853650985bcd40
Arwei0 <henson0220@gmail.com>
2025-08-16 23:24:57 +0800

    stock

--- test_push.py
+++ test_push.py
@@ -1,5 +1,12 @@
-# test_push.py
-from line_messaging_push import push_message
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+import os
 
-if __name__ == "__main__":
-    push_message("【測試】Messaging API 推播 OK ✅")
+# 從環境變數拿 LINE access token
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+USER_ID = "Ueeb38edab1a74313586d1bc43f792dbe"
+
+line_bot_api.push_message(USER_ID, TextSendMessage(text="測試推播成功！"))
+print("推播完成")
